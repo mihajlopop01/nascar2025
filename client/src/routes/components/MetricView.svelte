371bb61bc475380304e367a7a50b0588f0dee88c
kiboocodes <mihajlopopovic049@gmail.com>
2025-01-21 01:23:11 +0100

    Vraceni paneli. Ubacena promena template-a i timer

--- client/src/routes/components/MetricView.svelte
+++ client/src/routes/components/MetricView.svelte
@@ -1,6 +1,165 @@
 <script>
 	import { onMount } from 'svelte';
 
+
+	// const metrics = [
+	// 	'Car Stop',
+	// 	'RS Up',
+	// 	'RS Drop',
+	// 	'LS Up',
+	// 	'LS Drop',
+	// 	'RF Nut off Start',
+	// 	'RF Clear',
+	// 	'RF Mount',
+	// 	'RF Nut on finish',
+	// 	'LF Nut off Start',
+	// 	'LF Clear',
+	// 	'LF Mount',
+	// 	'LF Nut on finish',
+	// 	'RR Nut off Start',
+	// 	'RR Clear',
+	// 	'RR Mount',
+	// 	'RR Nut on finish',
+	// 	'LR Nut off Start',
+	// 	'LR Clear',
+	// 	'LR Mount',
+	// 	'LR Nut on finish',
+	// 	'RS Peg',
+	// 	'Dropoff',
+	// 	'LS Peg'
+	// ];
+
+	// let metrics2 = [
+	// 	{
+	// 		tag: 'General',
+	// 		metrics: {
+	// 			'Car Stop': null,
+	// 			'RS Up': null,
+	// 			'RS Drop': null,
+	// 			'LS Up': null,
+	// 			'LS Drop': null,
+	// 			'Car Goes': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'RF',
+	// 		metrics: {
+	// 			'Nut Off Start': 12.33,
+	// 			Clear: null,
+	// 			Mount: null,
+	// 			'Not On Finish': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'LF',
+	// 		metrics: {
+	// 			'Nut Off Start': null,
+	// 			Clear: null,
+	// 			Mount: null,
+	// 			'Not On Finish': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'RR',
+	// 		metrics: {
+	// 			'Nut Off Start': null,
+	// 			Clear: null,
+	// 			Mount: null,
+	// 			'Not On Finish': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'LR',
+	// 		metrics: {
+	// 			'Nut Off Start': null,
+	// 			Clear: null,
+	// 			Mount: null,
+	// 			'Not On Finish': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'Pegs',
+	// 		metrics: {
+	// 			'RS Peg': null,
+	// 			Dropoff: null,
+	// 			'LS Peg': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'Fuel',
+	// 		metrics: {
+	// 			'Can1 In': null,
+	// 			'Can1 Out': null,
+	// 			'Can2 In': null,
+	// 			'Can2 Out': null,
+	// 			'Fuel Added': null,
+	// 			'Can1 Valid': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'Other',
+	// 		metrics: {
+	// 			Other: null,
+	// 			Category: null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'RF',
+	// 		metrics: {
+	// 			'Nut On Finish': null,
+	// 			Pull: null,
+	// 			'Nut On Start': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'LF',
+	// 		metrics: {
+	// 			'Nut On Finish': null,
+	// 			Pull: null,
+	// 			'Nut On Start': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'RR',
+	// 		metrics: {
+	// 			'Nut On Finish': null,
+	// 			Pull: null,
+	// 			'Nut On Start': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'LR',
+	// 		metrics: {
+	// 			'Nut On Finish': null,
+	// 			Pull: null,
+	// 			'Nut On Start': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'Wrench',
+	// 		metrics: {
+	// 			'RS Set': null,
+	// 			'RS Complete': null,
+	// 			'LS Set': null,
+	// 			'LS Complete': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'Position',
+	// 		metrics: {
+	// 			'Sign X': null,
+	// 			'Car X': null,
+	// 			'LF Y': null,
+	// 			'LR Y': null
+	// 		}
+	// 	},
+	// 	{
+	// 		tag: 'Exit',
+	// 		metrics: {
+	// 			'Car Exit': null
+	// 		}
+	// 	}
+	// ];
 	const metrics = [
 		'Car Stop',
 		'RS Up',
@@ -28,157 +187,56 @@
 		'LS Peg'
 	];
 
-	let metrics2 = [
-		{
-			tag: 'General',
-			metrics: {
-				'Car Stop': null,
-				'RS Up': null,
-				'RS Drop': null,
-				'LS Up': null,
-				'LS Drop': null,
-				'Car Goes': null
-			}
-		},
-		{
-			tag: 'RF',
-			metrics: {
-				'Nut Off Start': 12.33,
-				Clear: null,
-				Mount: null,
-				'Not On Finish': null
-			}
-		},
-		{
-			tag: 'LF',
-			metrics: {
-				'Nut Off Start': null,
-				Clear: null,
-				Mount: null,
-				'Not On Finish': null
-			}
-		},
-		{
-			tag: 'RR',
-			metrics: {
-				'Nut Off Start': null,
-				Clear: null,
-				Mount: null,
-				'Not On Finish': null
-			}
-		},
-		{
-			tag: 'LR',
-			metrics: {
-				'Nut Off Start': null,
-				Clear: null,
-				Mount: null,
-				'Not On Finish': null
-			}
-		},
-		{
-			tag: 'Pegs',
-			metrics: {
-				'RS Peg': null,
-				Dropoff: null,
-				'LS Peg': null
-			}
-		},
-		{
-			tag: 'Fuel',
-			metrics: {
-				'Can1 In': null,
-				'Can1 Out': null,
-				'Can2 In': null,
-				'Can2 Out': null,
-				'Fuel Added': null,
-				'Can1 Valid': null
-			}
-		},
-		{
-			tag: 'Other',
-			metrics: {
-				Other: null,
-				Category: null
-			}
-		},
-		{
-			tag: 'RF',
-			metrics: {
-				'Nut On Finish': null,
-				Pull: null,
-				'Nut On Start': null
-			}
-		},
-		{
-			tag: 'LF',
-			metrics: {
-				'Nut On Finish': null,
-				Pull: null,
-				'Nut On Start': null
-			}
-		},
-		{
-			tag: 'RR',
-			metrics: {
-				'Nut On Finish': null,
-				Pull: null,
-				'Nut On Start': null
-			}
-		},
-		{
-			tag: 'LR',
-			metrics: {
-				'Nut On Finish': null,
-				Pull: null,
-				'Nut On Start': null
-			}
-		},
-		{
-			tag: 'Wrench',
-			metrics: {
-				'RS Set': null,
-				'RS Complete': null,
-				'LS Set': null,
-				'LS Complete': null
-			}
-		},
-		{
-			tag: 'Position',
-			metrics: {
-				'Sign X': null,
-				'Car X': null,
-				'LF Y': null,
-				'LR Y': null
-			}
-		},
-		{
-			tag: 'Exit',
-			metrics: {
-				'Car Exit': null
-			}
-		}
-	];
-
 	const metricData = metrics.map((metric) => ({
 		display: metric,
 		attr: metric.toLowerCase().replace(/\s+(.)/g, (_, c) => c.toUpperCase())
 	}));
 
-	export let currentCellIndex = -1;
+	// export let currentCellIndex = -1;
+	// export let selectedTemplate = '4 Tire';
+	let { currentCellIndex, selectedTemplate } = $props();
+
+  // Cuvamo vrednosti metrika kako bismo mogli da sacuvamo metrike kada se promeni template
+     let metricValues = $state({});
+
+	const templateMetrics = {
+		'4 Tire': metrics,
+		'2 RS': ['Car Stop','RS Up','RS Drop','RF Nut off Start','RF Clear','RF Mount','RF Nut on finish','RR Nut off Start','RR Clear','RR Mount','RR Nut on finish','RS Peg','Dropoff',],
+		'2 LS': ['Car Stop','LS Up','LS Drop','LF Nut off Start','LF Clear','LF Mount','LF Nut on finish','LR Nut off Start','LR Clear','LR Mount','LR Nut on finish','Dropoff','LS Peg'],
+		'Fuel Only': ['Car Stop']
+	};
+
+	// $:metricsToDisplay = templateMetrics[selectedTemplate];
+
+	// $: {
+	// 	metricsToDisplay.forEach(metric => {
+	// 		if (!(metric in metricValues)) {
+	// 			metricValues[metric] = ''; 
+	// 		}
+	// 	});
+	// }
+
+    //Metrike koje se prikazuju se menjaju kada god se promeni template
+	let metricsToDisplay = $derived(templateMetrics[selectedTemplate]);
+
+	// Kada se metricToDisplay promeni (odnosno kad se promeni template) proveravamo da li vec postoji vrednost za svaku metriku. Btw Charli cuckuje Hamiltona sledece sezone
+	$effect(() => {
+  		metricsToDisplay.forEach(metric => {
+		if (!(metric in metricValues)) {
+	  	metricValues[metric] = '';
+		}
+  	});
+	});
 
 	function updateTimeDisplay(event) {
+		//timeDisplay je primeljne od VideoPlayer komponente preko custom eventa
 		const { timeDisplay } = event.detail;
-		if (currentCellIndex >= 0 && currentCellIndex < metricData.length) {
-			const cell = document.querySelector(`td[name="${metricData[currentCellIndex].attr}"]`);
-			if (cell) {
-				cell.textContent = timeDisplay;
-				//ovde moze logika za cuvanje metrike u bazi
-				currentCellIndex++;
-
-				if (currentCellIndex >= metricData.length) {
-					currentCellIndex = metricData.length - 1;
-				}
+		if (currentCellIndex >= 0 && currentCellIndex < metricsToDisplay.length) {
+			const metric = metricsToDisplay[currentCellIndex];
+			metricValues[metric] = timeDisplay;
+			currentCellIndex++;
+			if (currentCellIndex >= metricsToDisplay.length) {
+				currentCellIndex = metricsToDisplay.length - 1;
 			}
 		}
 	}
@@ -189,11 +247,9 @@
 	//funkcija za brisanje vrednosti metrike. Metrike se brisu na desni klik. Lako mozemo da promenimo ako hoces
 	function handleCellRightClick(event, index) {
 		event.preventDefault();
-		if (index >= 0 && index < metricData.length) {
-			const cell = document.querySelector(`td[name="${metricData[index].attr}"]`);
-			if (cell) {
-				cell.textContent = '';
-			}
+		if (index >= 0 && index < metricsToDisplay.length) {
+			const metric = metricsToDisplay[index];
+			metricValues[metric] = '';
 		}
 	}
 
@@ -205,15 +261,15 @@
 <div class="metric-container">
 	<table>
 		<tbody>
-			{#each metricData as { display, attr }, index}
+			{#each metricsToDisplay as metric, index}
 				<tr>
-					<td>{display}</td>
+					<td>{metric}</td>
 					<td
-						name={attr}
+						name={metric.toLowerCase().replace(/\s+(.)/g, (_, c) => c.toUpperCase())}
 						class={index === currentCellIndex ? 'highlight' : ''}
 						onclick={() => handleCellClick(index)}
 						oncontextmenu={(event) => handleCellRightClick(event, index)}
-					></td>
+					>{metricValues[metric]}</td>   <!--Upisuje vrednost metrike u polje, ako vrednost postoji-->
 				</tr>
 			{/each}
 		</tbody>
